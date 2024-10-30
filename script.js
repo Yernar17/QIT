e96c05e9c99d95fd30e5f1a9cce82e62ca676dbb
Yernar Mynzhasar <ernarmynzhasar8@gmail.com>
2024-10-30 18:33:04 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var name = "Yernar";
+var surname = "Mynzhasar";
+var birthday = "20.12.2008";
+var otan = "Kazakhstan";
+
+console.log("Menin atim", name);
+console.log("Menin tgim", surname);
+console.log("Tugan Kunim", birthday);
+console.log("Menin Otanim", otan);
